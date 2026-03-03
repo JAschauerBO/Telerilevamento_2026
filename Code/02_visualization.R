1a8728c2ab04f7214f264777136af862aedd6776
Jakob Aschauer @Unibo <jakob.aschauer@studio.unibo.it>
2026-03-03 17:45:42 +0100

    Add visualization code for multispectral data

--- Code/02_visualization.R
+++ Code/02_visualization.R
@@ -1,5 +1,13 @@
 # R Code for visualizing multispectral data
 
+install.packages("devtools")
+install.packages("viridis")
+
+library(devtools)
+install_github("ducciorocchini/imageRy")
+
+library(viridis)
+
 library(terra) #use package terra for using spatial data
 library(imageRy) #package devoted to satellite images, functions start with im.
 
@@ -8,3 +16,39 @@ im.list()
 # Sentinel-2 bands
 # https://gisgeography.com/sentinel-2-bands-combinations/
 
+b2 <- im.import("sentinel.dolomites.b2.tif")
+
+
+# Changing colors
+# overview of colors in https://r-charts.com/colors/
+# use c()
+cl <- colorRampPalette(c("lightsalmon", "magenta", "mediumpurple1"))(100)
+# 100 classes
+plot(b2, col=cl) # to plot, use library(terra) before
+
+
+# less classes:
+cl <- colorRampPalette(c("lightsalmon", "yellow", "mediumpurple1"))(3)
+plot(b2, col=cl)
+
+# using viridis to change colors for daltonists; inferno is a colorramp by viridis
+plot(b2, col=inferno(100)) #col=mako(100)
+
+# Exercise: Assign a greycolor palette to the image
+cl_exercise <- colorRampPalette(c("black", "white"))(100)
+b3 <- im.import("sentinel.dolomites.b3.tif")
+b4 <- im.import("sentinel.dolomites.b4.tif")
+plot(b2, col=cl_exercise)
+plot(b3, col=cl_exercise)
+plot(b4, col=cl_exercise)
+
+
+# par (for multiframe (mf))
+par(mfrow=c(1,2))
+plot(b2, col=inferno(100))
+plot(b2, col=cl_exercise)
+
+dev.off() # close windows
+
+# instead of par we can use the function from imageRy
+im.multiframe(1,2)
