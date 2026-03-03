21dcee218369303eb4fe443d013904f869a3161d
Jakob Aschauer @Unibo <jakob.aschauer@studio.unibo.it>
2026-03-03 16:55:19 +0100

    Create 02_visualization.R for multispectral data visualization
    Add R script for visualizing multispectral data using terra and imageRy packages.

---
+++ Code/02_visualization.R
@@ -0,0 +1,10 @@
+# R Code for visualizing multispectral data
+
+library(terra) #use package terra for using spatial data
+library(imageRy) #package devoted to satellite images, functions start with im.
+
+im.list()
+
+# Sentinel-2 bands
+# https://gisgeography.com/sentinel-2-bands-combinations/
+
