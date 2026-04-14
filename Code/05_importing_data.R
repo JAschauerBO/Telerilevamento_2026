7fe9acadd765af4800d42bc96432481408c97e03
Jakob Aschauer @Unibo <jakob.aschauer@studio.unibo.it>
2026-04-14 17:32:35 +0200

    Enhance data importing script with export features
    Added code to export figures in PNG and PDF formats, and included NDVI calculations.

--- Code/05_importing_data.R
+++ Code/05_importing_data.R
@@ -31,3 +31,40 @@ plot(stack)
 # False Color Image
 plotRGB(stack, r=3, g=2, b=1) # ERROR like this! 
 plotRGB(stack, r=3, g=2, b=1, stretch="lin")
+
+#NDVI
+ndvi <- im.ndvi(stack, 3, 2)
+plot(ndvi)
+
+#export data: Case sensitive
+writeRaster(ndvi, "ndvi.tif")
+
+#Exporting a figure, not data!!!
+png("figure1.png")
+im.multiframe(2,2)
+plot(gre)
+plot(red)
+plot(nir)
+plot(ndvi)
+dev.off()
+
+pdf("figure1.pdf")
+im.multiframe(2,2)
+plot(gre)
+plot(red)
+plot(nir)
+plot(ndvi)
+dev.off()
+
+
+# Patchwork
+p1 <- im.ggplot(ndvi)
+p2 <- im.ridgeline(ndvi, scale=1)
+p1 + p2
+
+pdf("figure2.pdf)
+p1 <- im.ggplot(ndvi)
+p2 <- im.ridgeline(ndvi, scale=1)
+p1 + p2
+
+gitnir <- rast("https://raw.githubusercontent.com/ducciorocchini/Telerilevamento_2026/main/Drone2/DJI_20260409152942_0001_MS_NIR.TIF") # change "github.com" with "raw.githubusercontent.com" & delete "tree/"
