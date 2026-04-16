ab55ea9e954a3da0ad1e7107c3c67a98a25edf29
Jakob Aschauer @Unibo <jakob.aschauer@studio.unibo.it>
2026-04-16 15:34:48 +0200

    Add image processing code in 06_exam_example.R

--- Code/06_exam_example.R
+++ Code/06_exam_example.R
@@ -1,1 +1,22 @@
 library(terra)
+library(imageRy)
+
+setwd("C:/Users/jakob/Seafile/Meine Bibliothek/Erasmus/Telerilevamento/Coding")
+
+getwd()
+
+list.files()
+
+
+richat <- rast("richatstructure_oli_20260306.jpg")
+richat <- flip(richat)
+
+png("figure1.png")
+plot(richat)
+dev.off()
+
+png("figure_bande.png")
+im.multiframe(2,1)
+plot(richat[[1]])
+plot(richat[[2]])
+dev.off()
