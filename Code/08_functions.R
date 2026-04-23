ae41f01a8ef83dfbdc8f0504fd172e922dbc74bb
Jakob Aschauer @Unibo <jakob.aschauer@studio.unibo.it>
2026-04-23 15:02:07 +0200

    Create functions for sum and difference operations
    Added functions for summation and difference calculations.

---
+++ Code/08_functions.R
@@ -0,0 +1,16 @@
+## We learn how to create functions, YIPPIEH!
+
+somma <- function(x,y) {  # dont use sum because it overwrites the r function sum
+  z = x + y
+  return(z)
+  }
+
+somma(1,5)
+
+# Exercise: make a function called difference
+difference <- function(x,y) {
+  z = x - y
+  return(z)
+  }
+
+difference(5,3)
